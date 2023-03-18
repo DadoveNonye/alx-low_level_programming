43fe5353ceb25cc5b8d97968505fce19f58af780
dadovenonye <chinonyenmecha@yahoo.com>
2023-03-18 03:27:52 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,16 +6,17 @@
  * Return: 0.
  */
 
-	int main(void)
-	{
-		char alpha;
+int main(void)
+{
+	char alpha;
 
-		for (alpha = "a"; alpha <= "z"; alpha++)
-		       putchar(alpha);
-		for (alpha ="A"; alpha <= "Z"; alpha++)
-			putchar(alpha);
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
 
-		putchar("\n");
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+		putchar(alpha);
 
-		return (0);	
-	}
+	putchar('\n');
+
+	return (0);
+}
