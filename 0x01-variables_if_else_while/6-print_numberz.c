be0bb482635c47a3e9c0cd587b76b2c4fc4b1c31
dadovenonye <chinonyenmecha@yahoo.com>
2023-03-18 07:14:59 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -12,7 +12,8 @@ int main(void)
 	int singledigits;
 
 	for (singledigits = 0; singledigits < 10; singledigits++)
-		putchar((singledigits % 10) + "0");
+
+		putchar((singledigits % 10) + '0');
 	putchar("\n");
 	return (0);
 }
