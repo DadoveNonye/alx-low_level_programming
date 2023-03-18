a64152e05f04fe763ece074aeb23b8772954a5c8
dadovenonye <chinonyenmecha@yahoo.com>
2023-03-18 06:56:06 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+/* 6-print_numberz.c */
+
+#include <stdio.h>
+
+/**
+ * main - single digits of base 10 followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	int singledigits;
+
+	for (singledigits = 0; singledigits < 10; singledigits++)
+		putchar("%d", singledigits);
+	putchar("\n");
+	return (0);
+}
