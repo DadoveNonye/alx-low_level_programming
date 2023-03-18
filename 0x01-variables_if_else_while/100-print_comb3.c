46285d5b7b6395c748ef1859fe1768c46c672c20
dadovenonye <chinonyenmecha@yahoo.com>
2023-03-18 16:58:38 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,9 +17,9 @@ int main(void)
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if (num1 == 9 && num2 == 9)
+			if (num1 == 8 && num2 == 9)
 				continue;
-			
+
 			putchar(',');
 			putchar(' ');
 		}
