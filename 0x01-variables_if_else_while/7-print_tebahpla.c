af30abeab807371ccaaa54038c2da733780ea974
dadovenonye <chinonyenmecha@yahoo.com>
2023-03-18 08:23:21 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+/* 7-print_tebahpla.c */
+
+#include <stdio.h>
+
+/**
+ * main - print alphabets in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char reverse;
+
+		for (reverse = "z"; reverse <= "a"; reverse++)
+			putchar(reverse);
+		putchar("/n");
+			
+			return (0);
+}
+
