6b01e71941c28a0352c31bea67e65fae56547e11
dadovenonye <chinonyenmecha@yahoo.com>
2023-03-18 15:10:13 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,11 +13,11 @@ int main(void)
 	char alpha;
 
 		for (base16 = 0; base16 <= 10; base16++)
-			putchar ((base16 % 10) + ' ');
+			putchar ((base16 % 10) + '0');
 
 		for (alpha = 'a'; alpha <= 'f'; alpha++)
 			putchar (alpha);
-		/*	putchar ("\n")*/
+			putchar ("\n");
 
 		return (0);
 }
