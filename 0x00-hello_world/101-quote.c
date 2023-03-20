0118d97848ca69c7b766252017574aaefe60ded7
dadovenonye <chinonyenmecha@yahoo.com>
2023-03-19 19:10:53 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/* 101-quote.c */
+#include <stdio.h>
+
+/**
+ * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+		return(1);
+}
