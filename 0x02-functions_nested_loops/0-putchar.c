d271f0b01527bf8d61550a8fe4e8e702f08afcd5
dadovenonye <chinonyenmecha@yahoo.com>
2023-03-21 20:36:37 -0700

    0-putchar-c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,26 @@
-/* 0-putchar.c */
+/*
+ * File: 0-putchar.c
+ *
+ */
 
 #include "main.h"
 
 /**
- * main - program that prints _putchar
- * Return: 0
+ * main - Prints putchar followed by a new line.
+ *
+ * Return: 0.
  */
-
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
