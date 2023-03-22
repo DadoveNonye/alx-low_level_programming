f7fe73e720635e2f9ff4379c03c2be67a7f0b89d
dadovenonye <chinonyenmecha@yahoo.com>
2023-03-21 20:58:51 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+/* 1-alphabet.c */
+
+#include "main.h"
+
+/**
+ * main - program that prints alphabts in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = "a", i <= "z" ; i++)
+		_putchar(i);
+		_putchar(i);
+
+		return (0);
+}
