2afe1f19fbe8ebb51c073425b9b932a88ab0e486
dadovenonye <chinonyenmecha@yahoo.com>
2023-03-22 17:35:25 -0700

    updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,11 @@ void print_alphabet_x10(void)
 	char i;
 	int count = 0;
 	while (count++ <= 9)
-	{	
+
+	{
 	for (i = 'a'; i <= 'z'; i++)
-	
-		_putchar ('0');
+
+		_putchar ('i');
 	_putchar ('\n');
 	}
 }
