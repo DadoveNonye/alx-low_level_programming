d263133ac518f690677f7e8ad92ecde4d6093cad
dadovenonye <chinonyenmecha@yahoo.com>
2023-03-22 17:29:50 -0700

    updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,19 +3,18 @@
 
 /**
  * print_alphabet_x10 - program that prints alphebet 10x
- * 
+ *
  */
 
 void print_alphabet_x10(void)
 {
 	char i;
 	int count = 0;
-
+	while (count++ <= 9)
+	{	
 	for (i = 'a'; i <= 'z'; i++)
-	{
-		if (count < 10)
-		_putchar (i);
-	}
+	
+		_putchar ('0')
 	_putchar ('\n');
-
+	}
 }
