02672e8cd44fac9acf84a3344abf445aebecd729
dadovenonye <chinonyenmecha@yahoo.com>
2023-03-25 17:02:04 -0700

    updated

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _isupper - script that checks if c is uppercase
+ * @c: value to be checked
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 'A'; i <=  'Z'; i++)
+
+		if (c == 'C')
+		{
+			return (1);
+		else
+
+	return (0);
+	}
+}
