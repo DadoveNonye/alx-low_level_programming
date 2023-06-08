ff6e3e6f085f37ff6c2d7f8b2e0c8d722fdbc26c
DadoveNonye <chinonyenmecha@gmail.com>
2023-06-08 14:48:56 -0700

    update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: input string
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s);
+	{
+		_putchar(*s);
+	}
+	else
+		_putchar("\n");
+}
